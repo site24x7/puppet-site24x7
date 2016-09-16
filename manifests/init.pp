f7e02e87cfbd690f4c9dedae359b41122d195309
site24x7 <site24x7@users.noreply.github.com>
2016-09-16 11:51:45 +0530

    apikey changed to devicekey

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,4 @@
-class site24x7($api_key=$site24x7::params::site24x7agent_apikey,$agent_uninstaller=$site24x7::params::site24x7agent_uninstall,$agent_proxy=$site24x7::params::site24x7agent_proxy) inherits site24x7::params{
+class site24x7($device_key=$site24x7::params::site24x7agent_devicekey,$agent_uninstaller=$site24x7::params::site24x7agent_uninstall,$agent_proxy=$site24x7::params::site24x7agent_proxy) inherits site24x7::params{
 
 notify{'site24x7 linux monitoring agent installation started':}
 
